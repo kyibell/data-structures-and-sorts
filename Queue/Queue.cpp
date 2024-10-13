784a356158240f2309385df84950c8b07ca4f693
kyibell <bellkyilee@gmail.com>
2024-10-13 15:35:47 -0500

    Finished Stack implementation and working on Queue implementation.

---
+++ Queue/Queue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_CAPACITY = 100; 
+
+// Queue class
+class Queue {
+    public:
+
+    int queue[MAX_CAPACITY];
+    int front = -1;
+    int rear = -1;
+
+    // isEmpty
+    bool isEmpty() {
+        return false;
+    }
+    // isFull
+    //Enqueue
+    // Dequeue
+    // front
+    // back
+    // size
+};
